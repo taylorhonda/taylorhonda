b4dbb4bb87c2af826d0b77e09b885671886c51c3
Taylor Honda <taylorhonda@Taylors-MacBook-Air.local>
2016-04-26 02:55:02 -0700

    First Draft
    Yay, first draft

---
+++ assets/js/script.js
@@ -0,0 +1,144 @@
+$(document).ready(function(){
+	
+	$(".pages").hover(
+		
+		function() {
+		$(this).css({"color":"#20b2aa"});
+	},
+	function() {
+		$(this).css({"color":"black"});
+	});
+
+	$("#title").hover(
+		
+		function() {
+		$(this).css({"color":"#20b2aa"});
+	},
+	function() {
+		$(this).css({"color":"black"});
+	});
+
+
+	$("#design1").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin1").fadeIn();
+	
+	});
+
+	$("#design2").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin2").fadeIn();
+
+	});
+
+	$("#design3").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin3").fadeIn();
+	});
+
+	$("#design4").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin4").fadeIn();
+	});
+
+	$("#design5").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin5").fadeIn();
+	});
+
+	$("#design6").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin6").fadeIn();
+	});
+
+	$("#design7").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin7").fadeIn();
+	});
+
+	$("#design8").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin8").fadeIn();
+	});
+
+	$("#photo1").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin9").fadeIn();
+
+	});
+
+	$("#photo2").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin10").fadeIn();
+
+	});
+
+	$("#photo3").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin11").fadeIn();
+
+	});
+
+	$("#photo4").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin12").fadeIn();
+
+	});
+
+	$("#photo5").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin13").fadeIn();
+
+	});
+
+	$("#photo6").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin14").fadeIn();
+
+	});
+
+	$("#photo7").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin15").fadeIn();
+
+	});
+
+	$("#photo8").click(function() {
+		$(".overlay").fadeIn();
+		$("#zoomin16").fadeIn();
+
+	});
+
+	$(".overlay").click(function() {
+		$(".overlay").fadeOut();
+		$("#zoomin1").fadeOut();
+		$("#zoomin2").fadeOut();
+		$("#zoomin3").fadeOut();
+		$("#zoomin4").fadeOut();
+		$("#zoomin5").fadeOut();
+		$("#zoomin6").fadeOut();
+		$("#zoomin7").fadeOut();
+		$("#zoomin8").fadeOut();
+		$("#zoomin9").fadeOut();
+		$("#zoomin10").fadeOut();
+		$("#zoomin11").fadeOut();
+		$("#zoomin12").fadeOut();
+		$("#zoomin13").fadeOut();
+		$("#zoomin14").fadeOut();
+		$("#zoomin15").fadeOut();
+		$("#zoomin16").fadeOut();
+
+	});
+
+	$("#selfie").click(function() {
+		if ($("#selfie").hasClass("opacity")) {
+			$("#selfie").removeClass("opacity");
+		}
+		else {
+			$("#selfie").addClass("opacity");
+		}
+	});
+
+
+
+});
